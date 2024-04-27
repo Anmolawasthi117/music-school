5cf1e8e8c03c8fdcfb150ae8a0e12e1e6c08414d
Anmol <anmolawasthi117@gmail.com>
2024-04-27 18:30:04 +0530

    home page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 
-
+import HeroSection from "@/components/HeroSection";
+import FeaturedCourses from "@/components/FeaturedCourses";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 export default function Home() {
   return (
-    <div>
-    <h1>anmol with chai aur code</h1>
-    </div>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+      <FeaturedCourses />
+       <WhyChooseUs />
+      <MusicSchoolTestimonials/>
+      <UpcomingWebinars/>
+    </main>
+
   );
 }
