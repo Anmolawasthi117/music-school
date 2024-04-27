b27acb8655c6bfe97e2ac0b16408c9ed54cbba04
Anmol <anmolawasthi117@gmail.com>
2024-04-27 22:26:27 +0530

    main work done project completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import FeaturedCourses from "@/components/FeaturedCourses";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
+import Instructors from "@/components/Instructor";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
@@ -12,6 +14,8 @@ export default function Home() {
        <WhyChooseUs />
       <MusicSchoolTestimonials/>
       <UpcomingWebinars/>
+      <Instructors/>
+      <Footer/>
     </main>
 
   );
